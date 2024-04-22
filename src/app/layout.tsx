e1848f1b5a75144eb67462b3500fdd3c9102d30a
hayama <hayamaroom@gmail.com>
2024-04-22 08:09:48 +0800

    add theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Agbalumo as FontBrand, Roboto as Font } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme";
 
 const font = Font({
   weight: ["100" , "300" , "400" , "500" , "700" , "900"],
@@ -26,12 +27,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(
         'min-h-screen bg-background/5 text-foreground antialiased',
         font.variable, brand.variable
       )}>
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
