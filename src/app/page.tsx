7f34e956aa2fb16a1f801e26fbf7adec5ceb4c59
hayama <hayamaroom@gmail.com>
2024-04-22 15:29:10 +0800

    change page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between border-b border-border pb-4 mb-4">
         <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
         <div className="flex items-center space-x-2">
           <div className='font-semibold py-3 flex items-center gap-2'>
